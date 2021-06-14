5501ff5f0ac7c02eae5e532e2e5386c10a50858a
Adrici <cirellia5@gmail.com>
2021-06-14 18:18:34 -0300

    Implementando Validação customizada com Micronaut Validation

---
+++ src/main/kotlin/carros/Carro.kt
@@ -0,0 +1,10 @@
+package carros
+
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.NotBlank
+
+@Introspected
+data class Carro (@field:NotBlank val modelo: String?,
+                  @field:NotBlank @field:Placa val placa: String?) {
+
+}
